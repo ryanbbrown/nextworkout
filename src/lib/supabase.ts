470ad6610c24fd1894a82035d378d31ffc96d56e
Ryan Brown <ryanb4257@gmail.com>
2025-03-29 12:20:59 -0400

    added supabase items

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://oncudjiacscrgcprmrgk.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uY3VkamlhY3NjcmdjcHJtcmdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNjQ3NjAsImV4cCI6MjA1ODg0MDc2MH0.I-llsTbLUHQiwZtv5yH228gZ1N_0Ja1zN1CWpLdFPoY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
